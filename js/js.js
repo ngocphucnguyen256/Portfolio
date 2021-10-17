ed2b3420cda031ec3cb4ce9b5e288322c8e0aaa1
ngocphucnguyen256 <69982260+ngocphucnguyen256@users.noreply.github.com>
2021-10-17 21:24:34 +0700

    Animation go brrr

--- js/js.js
+++ js/js.js
@@ -4,74 +4,49 @@ $(window).on("load", function(){
         color: '#085875',
         connectParticles: true,
       });
+      var wow = new WOW(
+        {
+          boxClass:     'wow',      // animated element css class (default is wow)
+          animateClass: 'animated', // animation css class (default is animated)
+          offset:       0,          // distance to the element when triggering the animation (default is 0)
+          mobile:       true,       // trigger animations on mobile devices (default is true)
+          live:         true,       // act on asynchronously loaded content (default is true)
+          scrollContainer: null,    // optional scroll container selector, otherwise use window,
+          resetAnimation: true,     // reset animation on end (default is true)
+        }
+      );
+      wow.init();
 //   $('#load').delay(1000).fadeOut('slow', function(){
 //       $('body').delay(1000).removeClass('preloading');
 //   });
 
 })
-window.onload=function() {
-  
-    var slideIndex =0;
-    // slideShow();
-    navHightLight();
 
 
-    let navList = document.querySelectorAll(".nav-menu>ul>li>a");
-  
-        let sectionList = document.querySelectorAll("#top, #about, #resume, #project, #quotes, #contact");
-     
-
-        let lastId;
-        let cur = [];
-        // window.addEventListener("scroll", event => {
-        //     let fromTop = window.scrollY;
-          
-        //     navList.forEach(link => {
-        //       let section =link.hash;
-        //         console.log(section)
-        //        var a= document.getElementById(section)
-        //        console.log(a);
-        //       if (
-        //         section.offsetTop <= fromTop &&
-        //         section.offsetTop + section.offsetHeight > fromTop
-        //       ) {
-        //         link.classList.add("currentNav");
-        //       } else {
-        //         link.classList.remove("currentNav");
-        //       }
-        //     });
-        //   });
-
-
-    function slideShow() {
-        const slider = document.querySelector(".slider")
-        const sliderMain = document.querySelector(".slider__main")
-        const sliderItems=document.querySelectorAll("slider__item")
-        const leftButton= document.querySelector(".slider__button--left")
-        const rightButton= document.querySelector(".slider__button--right")
+// function slideShow() {
+//         const slider = document.querySelector(".slider")
+//         const sliderMain = document.querySelector(".slider__main")
+//         const sliderItems=document.querySelectorAll("slider__item")
+//         const leftButton= document.querySelector(".slider__button--left")
+//         const rightButton= document.querySelector(".slider__button--right")
 
     
-        if(slideIndex>=slideList.length){
-            slideIndex =0
-        }
-        if(slideIndex<0){
-            slideIndex =slideList.length
-        }
-        for(var i=0; i<slideList.length;i++){
-            slideList[i].classList.remove("show-slide")
-            thumbnailList[i].classList.remove("show-thumbnail")
-        }
-        slideList[slideIndex].classList.add("show-slide")
-        thumbnailList[slideIndex].classList.add("show-thumbnail")
-        slideIndex++;
-        setTimeout(slideShow, 4000);
-    }
-    function navHightLight(){
-    
-    }
+//         if(slideIndex>=slideList.length){
+//             slideIndex =0
+//         }
+//         if(slideIndex<0){
+//             slideIndex =slideList.length
+//         }
+//         for(var i=0; i<slideList.length;i++){
+//             slideList[i].classList.remove("show-slide")
+//             thumbnailList[i].classList.remove("show-thumbnail")
+//         }
+//         slideList[slideIndex].classList.add("show-slide")
+//         thumbnailList[slideIndex].classList.add("show-thumbnail")
+//         slideIndex++;
+//         setTimeout(slideShow, 4000);
+//     }
 
-
-}
 
 
 
