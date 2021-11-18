e79c35a5efea6a890381beda69caee625ddbf926
ngocphucnguyen256 <69982260+ngocphucnguyen256@users.noreply.github.com>
2021-11-18 21:30:17 +0700

    custom cursor, semantic html, overlay

--- js/js.js
+++ js/js.js
@@ -4,6 +4,19 @@ $(window).on("load", function(){
         color: '#085875',
         connectParticles: true,
       });
+      const cursor = document.querySelector('.cursor');
+
+      document.addEventListener('mousemove', e => {
+          cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+      })
+
+      document.addEventListener('click', () => {
+          cursor.classList.add("expand");
+
+          setTimeout(() => {
+              cursor.classList.remove("expand");
+          }, 500)
+      })
       var wow = new WOW(
         {
           boxClass:     'wow',      // animated element css class (default is wow)
